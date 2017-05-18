8c57eac51ec81c1b5b7bb1537612016c9eca6fd7
William Whitworth <william10charles@gmail.com>
2017-05-18 12:02:21 -0600

    building date algorithm

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 // Import our needed node modules
-require('es6-promise').polyfill;
-require('isomorphic-fetch');
-require('dotenv').config();
-const express = require('express');
+require("es6-promise").polyfill;
+require("isomorphic-fetch");
+require("dotenv").config();
+const express = require("express");
 
 // Initialize our app
 const app = express();
 // Set a const for our api key in .env
 const QUANDL_API_KEY = process.env.QUANDL_API_KEY;
-const baseUrl = 'https://quandl.com/api/v3/datatables/WIKI/PRICES.json';
+const baseUrl = "https://quandl.com/api/v3/datatables/WIKI/PRICES.json";
 
 // Set the port to 3001 instead of 3000
-app.set('port', process.env.PORT || 3001);
+app.set("port", process.env.PORT || 3001);
 
 // For later when we deploy to production, use the static
 // assets built in the client/build folder instead of
 // hosted at localhost:3000
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 
 // Extract checking the status of the response for reuse
@@ -38,15 +38,45 @@ function parseJSON(response) {
   return response.json();
 }
 
-app.get('/api/quandl/:date', (req, res, next) => {
-  console.log('Requesting stock data from QUANDL...');
+app.get("/api/quandl/:date", (req, res, next) => {
+  console.log("Requesting stock data from QUANDL...");
   let date = req.params.date;
 
   //let date = req.body.date;
   //?date=20160912&qopts.per_page=100&qopts.columns=ticker,date,close&api_key=soaV8Boit143qWZAubE4
-  let year = date.substring(0, 4);
-  let month = date.substring(4, 6);
-  let day = date.substring(6, 8);
+  let year = Number(date.substring(0, 4));
+  let month = Number(date.substring(4, 6));
+  let day = Number(date.substring(6, 8));
+
+  function dateGen(num) {
+    let newDay;
+    let newMonth;
+    let newYear;
+
+    num = Number(num);
+    if (day - num <= 0) {
+      if (month - 1 === 0) {
+        newMonth = 12;
+        newYear = year - 1;
+      } else {
+        newMonth = month - 1;
+        newYear = year;
+      }
+      newDay = 30 + (day - num);
+    } else {
+      newDay = day - num;
+      newMonth = month;
+      newYear = year;
+    }
+
+    if (newDay < 10) {
+      newDay = "0" + newDay.toString();
+    }
+    if (newMonth < 10) {
+      newMonth = "0" + newMonth.toString();
+    }
+    return newYear.toString() + newMonth.toString() + newDay.toString();
+  }
 
   //${baseUrl}?qopts.per_page=100&=qopts.columns=ticker,date,close&date=${date}&api_key=${QUANDL_API_KEY}
   //`${baseUrl}?qopts.per_page=100&=qopts.columns=ticker,date,close&date.lt=${date}&date.gt=&api_key=${QUANDL_API_KEY}`;
@@ -68,7 +98,34 @@ app.get('/api/quandl/:date', (req, res, next) => {
 
   promises.push(
     fetch(
-      `${baseUrl}?qopts.per_page=3&=qopts.columns=ticker,date,close&date=${date - 1}&api_key=${QUANDL_API_KEY}`
+      `${baseUrl}?qopts.per_page=3&=qopts.columns=ticker,date,close&date=${dateGen(1)}&api_key=${QUANDL_API_KEY}`
+    )
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(json => {
+        return json;
+      })
+      .catch(error => {
+        next(error);
+      })
+  );
+  promises.push(
+    fetch(
+      `${baseUrl}?qopts.per_page=3&=qopts.columns=ticker,date,close&date=${dateGen(7)}&api_key=${QUANDL_API_KEY}`
+    )
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(json => {
+        return json;
+      })
+      .catch(error => {
+        next(error);
+      })
+  );
+
+  promises.push(
+    fetch(
+      `${baseUrl}?qopts.per_page=3&=qopts.columns=ticker,date,close&date=${dateGen(30)}&api_key=${QUANDL_API_KEY}`
     )
       .then(checkStatus)
       .then(parseJSON)
@@ -81,20 +138,11 @@ app.get('/api/quandl/:date', (req, res, next) => {
   );
 
   Promise.all(promises).then(results => {
+    let scrubbedResult = results.map(day => {
+      return;
+    });
     res.json(results);
   });
-  // fetch(
-  //   `${baseUrl}?qopts.per_page=3&=qopts.columns=ticker,date,close&date.lt=${date}&date.gt=${date - 30}&api_key=${QUANDL_API_KEY}`
-  // )
-  //   .then(checkStatus)
-  //   .then(parseJSON)
-  //   .then(json => {
-  //     // Sends a json response from Express
-  //     res.json(json);
-  //   })
-  //   .catch(error => {
-  //     next(error);
-  //   });
 });
 
 // Defines next action for errors
@@ -107,6 +155,6 @@ function errorHandler(err, req, res, next) {
 // Tell the app to use the errorHandler middleware
 app.use(errorHandler);
 
-app.listen(app.get('port'), () => {
-  console.log(`Find the server at http://localhost:${app.get('port')}/`);
+app.listen(app.get("port"), () => {
+  console.log(`Find the server at http://localhost:${app.get("port")}/`);
 });
