28df4eb082b93cc2fd439a3b7141d7111c547107
Renzo Tomlinson <rttomlinson@gmail.com>
2017-05-18 15:02:04 +0000

    Add dateslider and navbar

---
+++ src/components/DateSlider.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DateSlider = () => {
+    return (
+        <div>
+            Date Slider
+        </div>
+    );
+};
+
+export default DateSlider;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+            <a className="navbar-brand" href="#">Rideligard Stocks</a>
+        </nav>
+    );
+    
+};
+
+export default Navbar;
+            
+            
+            
