89f4c9f8d8d51f11672204895f47e40a1ae67de3
Christian Florez <florez.christian@gmail.com>
2017-07-10 17:09:03 -0400

    add reducers

--- README.md
+++ README.md
@@ -2,9 +2,24 @@
 Buy low, sell high.
 
 Now...
-  1. Make "symbols" optional
-    -> if user does not provide any, create a Set from the api results;
+  1. have a default panel of stocks -> 
+      pick 12 random stocks and make these the default watchlist when you load the page
   2. create actions -> reducers (use swapi project for help setting up)
     that will update stocks state with api info
   3. create table component
   4. enable filtering via ticker
+  5. when we start adding transactions, we can add our stock to the stock watchlist
+
+
+  TRICKY: HOW DO WE SET UP FILTER?
+    -"Filter/Reset Buttons"
+    -Upon "filtering", we add it to watchlist, make api request, THEN we filter"
+    -Upon reset, we set stockFilter back to null value
+initialState: {
+  stockFilter: "",
+  stockWatchlist: [...defaultStocks],
+  stockData: apiResults,
+  date: datePicker
+}
+
+getInitialStockData("a,b,c,d")
---
+++ client/src/reducers.js
@@ -0,0 +1,77 @@
+import * as Actions from "./actions";
+import { combineReducers } from "redux";
+
+const initialState = {
+  date: "1999-11-18",
+  stockFilter: "",
+  stockWatchlist: [],
+  stockData: {
+    stocks: {},
+    isFetching: false,
+    error: null
+  }
+}
+
+const date = (state = initialState.date, action) => {
+  switch(action.type) {
+    case Actions.SET_date: 
+      return action.data
+    default:
+      return state;
+  }
+}
+
+const stockFilter = (state = initialState.stockFilter, action) => {
+  switch(action.type) {
+    case Actions.SET_FILTER: 
+      return action.data
+    case Actions.RESET_FILTER:
+      return "";
+    default:
+      return state;
+  }
+}
+
+const stockWatchlist = (state = initialState.stockWatchlist, action) => {
+  switch(action.type) {
+    case Actions.ADD_STOCK_TO_LIST:
+      return [
+        ...state,
+        action.data
+      ];
+    default:
+      return state;
+  }
+}
+
+const stockData = (state = initialState.stockData, action) => {
+  switch (action.type) {
+    case Actions.GET_STOCKS_SUCCESS:
+      return {
+        ...state,
+        stocks: action.data,
+        isFetching: false
+      };
+    case Actions.GET_STOCKS_REQUEST:
+      return {
+        ...state,
+        isFetching: true,
+        error: null
+      };
+    case Actions.GET_STOCKS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error
+      };
+    default:
+      return state;
+  }
+}
+
+export const stockApp = combineReducers({
+  date,
+  stockFilter,
+  stockWatchlist,
+  stockData
+});
