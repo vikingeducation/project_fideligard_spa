7f3c8b04e3eadba260a9605b4ac3e22b9107503c
Stephanie Barker <song_mage@yahoo.com>
2017-09-08 12:44:56 -0400

    how do I api

--- server/server.js
+++ server/server.js
@@ -6,21 +6,19 @@ const fetch = require("node-fetch");
 const BASE_URL = "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json";
 const API_KEY = process.env.API_KEY;
 
-// https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?date.gte=20150101&date.lt=20160101&api_key=[api key goes here]
-
 app.get("/api/prices", async (req, res) => {
-  try {
-    let response = await fetch(
-      `${BASE_URL}?date=20150105,20150106&qopts.columns=ticker,date,close&api_key=${API_KEY}`
-    );
-    let prices = await response.json();
-    console.log(prices);
-    res.json(prices);
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		let response = await fetch(
+			`${BASE_URL}?date=20150105&qopts.per_page=20&api_key=${API_KEY}`
+		);
+		let prices = await response.json();
+		console.log(JSON.stringify(prices, null, 2));
+		res.json(prices);
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 app.listen(3001, () => {
-  console.log("Server is now listening...");
+	console.log("Server is now listening...");
 });
