aa7a4a0d1d84a798d708ddc7fb7b0f53814d6249
Will Timpson <will.timpson@gmail.com>
2017-09-08 16:06:46 -0400

    DatePicker... Yay!

--- client/src/components/DateSlider.js
+++ client/src/components/DateSlider.js
@@ -1,7 +1,6 @@
 import React from "react";
+import DatePicker from "react-datepicker";
 
-const DateSlider = () => {
-  return <h1>Sliding! Weee!</h1>;
-};
+const DateSlider = () => <DatePicker placeholderText="Select a date" />;
 
 export default DateSlider;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "react-datepicker/dist/react-datepicker.css";
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import App from "./components/App";
