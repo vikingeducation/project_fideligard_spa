97db024ef5a2622fbeb5881e95d9d41350896b45
EdwinYung <eyung7@gmail.com>
2018-01-05 13:26:08 -0500

    merging

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
+import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
->>>>>>> bc3de846ab4f26c5782688afcabe669c025b229c
+
 registerServiceWorker();
