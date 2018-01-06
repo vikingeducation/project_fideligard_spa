8d3cd06fd100db09616b6d6b3afd200d614c402b
Austin1780 <austinwilliamsmith@gmail.com>
2018-01-05 17:20:01 -0700

    set date display

--- src/components/StockPanel.js
+++ src/components/StockPanel.js
@@ -7,34 +7,41 @@ const StockPanel = ({ data }) => {
   console.log(data);
   console.log("====================");
 
-  let dateIndex = 0;
+  let dateIndex = 100;
   let rows = [];
-  for (let h = 0; h < data.stocksArray.length; h++) {
-    if (data.stocksArray[h][0] === data.date) {
-      dateIndex = h;
-      console.log("++++++++++++++");
-      console.log(data.stocksArray[0][dateIndex]);
-      for (let i = 0; i < data.stocks.length; i++) {
-        rows.push(
-          <tr>
-            <th scope="row">{data.stocks[i]}</th>
-            <td>{data.stocksArray[i][dateIndex][1]}</td>
-            <td>{data.stocksArray[i][dateIndex - 2][1]}</td>
-            <td>{data.stocksArray[i][dateIndex - 7][1]}</td>
-            <td>{data.stocksArray[i][dateIndex - 30][1]}</td>
-            <td>Trade</td>
-          </tr>
-        );
-      }
-    }
+  for (let i = 0; i < data.stocks.length; i++) {
+    rows.push(
+      <tr>
+        <th scope="row">{data.stocks[i]}</th>
+        <td>{data.stocksArray[i][dateIndex][1]}</td>
+        <td>{data.stocksArray[i][dateIndex - 2][1]}</td>
+        <td>{data.stocksArray[i][dateIndex - 7][1]}</td>
+        <td>{data.stocksArray[i][dateIndex - 30][1]}</td>
+        <td>Trade</td>
+      </tr>
+    );
   }
+
+  // for (let h = 0; h < data.stocksArray[i].length; h++) {
+  //   console.log("\\\\\\\\\\\\\\\\\\");
+  //   console.log(data.stocksArray[100][0]);
+  //   console.log(data.date);
+  //   console.log("\\\\\\\\\\\\\\\\\\");
+  //   if (data.stocksArray[i][h][0] === data.date) {
+  //     dateIndex = h;
+  //     console.log("++++++++++++++");
+  //     console.log(data.stocksArray[i][0][dateIndex]);
+  //   }
+  // }
 
   console.log(rows);
 
   return (
     <div className="panel panel-default">
       <div className="panel-heading">
-        <h3 className="panel-title">Stocks</h3>
+        <h3 className="panel-title">
+          Stocks for {data.stocksArray[0][dateIndex][0]}
+        </h3>
         <div className="rightAlign">
           <label htmlFor="stockFilter">Filter:</label>
           <input type="text" name="stockFilter" />
